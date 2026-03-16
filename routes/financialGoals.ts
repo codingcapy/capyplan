ef01ee4ab55a8c87d50624e56a59b34d511711fd
PaulArkhet <102385105+codingcapy@users.noreply.github.com>
2026-03-16 11:06:24 -0700

    bug fix: passing type date into financial goals target date attribute in create api route

--- routes/financialGoals.ts
+++ routes/financialGoals.ts
@@ -8,51 +8,49 @@ import { db } from "../db";
 import { plans as plansTable } from "../schemas/plans";
 import { and, eq } from "drizzle-orm";
 import { HTTPException } from "hono/http-exception";
+import z from "zod";
+
+const createFinancialGoalSchema = createInsertSchema(financialGoalsTable, {
+  targetDate: z.coerce.date(), // accepts ISO strings and coerces to Date
+}).omit({
+  financialGoalId: true,
+  createdAt: true,
+});
 
 export const financialGoalsRouter = new Hono()
-  .post(
-    "/",
-    zValidator(
-      "json",
-      createInsertSchema(financialGoalsTable).omit({
-        financialGoalId: true,
-        createdAt: true,
-      }),
-    ),
-    async (c) => {
-      const decodedUser = requireUser(c);
-      const insertValues = c.req.valid("json");
-      const { result: plan, error: planError } = await mightFail(
-        db
-          .select()
-          .from(plansTable)
-          .where(
-            and(
-              eq(plansTable.planId, insertValues.planId),
-              eq(plansTable.userId, decodedUser.id),
-            ),
+  .post("/", zValidator("json", createFinancialGoalSchema), async (c) => {
+    const decodedUser = requireUser(c);
+    const insertValues = c.req.valid("json");
+    const { result: plan, error: planError } = await mightFail(
+      db
+        .select()
+        .from(plansTable)
+        .where(
+          and(
+            eq(plansTable.planId, insertValues.planId),
+            eq(plansTable.userId, decodedUser.id),
           ),
-      );
-      if (planError)
-        throw new HTTPException(500, { message: "Plan lookup failed" });
-      if (!plan || plan.length === 0)
-        throw new HTTPException(401, { message: "Unauthorized" });
-      const {
-        error: financialGoalInsertError,
-        result: financialGoalInsertResult,
-      } = await mightFail(
-        db.insert(financialGoalsTable).values(insertValues).returning(),
-      );
-      if (financialGoalInsertError) {
-        console.log("Error while creating financial goal");
-        throw new HTTPException(500, {
-          message: "Error while creating financial goal",
-          cause: financialGoalInsertError,
-        });
-      }
-      return c.json({ financialGoal: financialGoalInsertResult[0] }, 200);
-    },
-  )
+        ),
+    );
+    if (planError)
+      throw new HTTPException(500, { message: "Plan lookup failed" });
+    if (!plan || plan.length === 0)
+      throw new HTTPException(401, { message: "Unauthorized" });
+    const {
+      error: financialGoalInsertError,
+      result: financialGoalInsertResult,
+    } = await mightFail(
+      db.insert(financialGoalsTable).values(insertValues).returning(),
+    );
+    if (financialGoalInsertError) {
+      console.log("Error while creating financial goal");
+      throw new HTTPException(500, {
+        message: "Error while creating financial goal",
+        cause: financialGoalInsertError,
+      });
+    }
+    return c.json({ financialGoal: financialGoalInsertResult[0] }, 200);
+  })
   .get("/", async (c) => {
     const {
       result: financialGoalsQueryResult,
