8bd6b09cabfa96492d4c2813b62420d1a106522a
PaulArkhet <spkim0921@gmail.com>
2026-03-29 10:57:37 -0700

    feat: implement update year of birth and location

--- frontend/src/components/CountriesDropdown.tsx
+++ frontend/src/components/CountriesDropdown.tsx
@@ -1,14 +1,21 @@
 import { Plan } from "../../../schemas/plans";
+import { useUpdateLocationMutation } from "../lib/api/plans";
 import { countries } from "../lib/utils";
 
 export function CountriesDropdown(props: { plan: Plan }) {
+  const { mutate: updateLocation, isPending: updateLocationPending } =
+    useUpdateLocationMutation();
+
+  function handleUpdateLocation(l: string) {
+    if (updateLocationPending) return;
+    updateLocation({ planId: props.plan.planId, location: l });
+  }
+
   return (
     <div className="absolute top-8 left-[32%] border border-[#a0a0a0] bg-[#303030] custom-scrollbar h-[150px] overflow-y-auto">
       {countries.map((c) => (
         <div
-          //   onClick={() =>
-          //     updateCountry({ planId: props.plan.planId, location: c })
-          //   }
+          onClick={() => handleUpdateLocation(c)}
           className="pl-1 pr-5 cursor-pointer hover:bg-[#222222] transition-all ease-in-out duration-300"
         >
           {c}
